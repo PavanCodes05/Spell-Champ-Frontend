4a1bb2f16d318a4984366df9517533b9b885b70b
Madhumitha2k05 <madhumitha2k05@gmail.com>
2025-04-21 13:05:23 +0530

    Spell-Champ-Frontend

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ExerciseHomePage extends StatefulWidget {
   const ExerciseHomePage({super.key, required this.grade});
   final int grade;
@@ -13,18 +12,18 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
   final List<bool> taskCompleted = [true, true, false, true, false, false];
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFD3CFE3),
       body: SafeArea(
         child: Stack(
           children: [
             Padding(
-              padding: const EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 10),
+                  const SizedBox(height: 35),
                   const Text(
                     "Welcome back",
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color: Colors.black54),
@@ -33,7 +32,7 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
                     "Sheetaal Gandhi",
                     style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.black),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 40),
                   const Text("Daily Drill", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
                   const SizedBox(height: 10),
                   Row(
@@ -51,27 +50,31 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
                               if (taskCompleted[index])
                                 Image.asset(
                                   "assets/images/tick.png",
-                                  width: 40,  // Updated size
-                                  height: 40, // Updated size
+                                  width: 40,
+                                  height: 40,
                                 ),
                             ],
                           ),
                           const SizedBox(height: 5),
-                          Text(["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][index]),
+                          Text(
+                            ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][index],
+                            style: const TextStyle(
+                              color: Colors.white,
+                              shadows: [BoxShadow(color: Colors.grey, blurRadius: 10)],
+                            ),
+                          ),
                         ],
                       );
                     }),
                   ),
-                  GestureDetector(
-                    
-                    child: _buildLargeCard("assets/images/exercise.png"),
-                  ),
-                  const SizedBox(height: 15),
+                  const SizedBox(height: 30),
+                  _buildExerciseCard(),
+                  const SizedBox(height: 50),
                   Row(
                     children: [
-                      Expanded(child: _buildQuizCard("assets/images/quiz.png", "assets/images/quiz-font.png")),
+                      Expanded(child: _buildQuizCard()),
                       const SizedBox(width: 10),
-                      Expanded(child: _buildResultCard("assets/images/result.png", "assets/images/result-font.png")),
+                      Expanded(child: _buildResultCard()),
                     ],
                   ),
                   const Spacer(),
@@ -92,8 +95,8 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
                       color: Colors.white,
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 2,
+                          color: Colors.grey,
+                          blurRadius: 10,
                           offset: Offset(0, 2),
                         ),
                       ],
@@ -115,6 +118,7 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
                         "50",
                         style: TextStyle(
                           color: Colors.white,
+                          shadows: [BoxShadow(color: Colors.grey, blurRadius: 10)],
                           fontSize: 10,
                           fontWeight: FontWeight.bold,
                         ),
@@ -124,31 +128,26 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
                 ],
               ),
             ),
+
+            
             Positioned(
-              bottom: 20,
-              left: 20,
-              right: 20,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 79, 17, 150),
-                  borderRadius: BorderRadius.circular(16),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 8,
-                      offset: const Offset(0, 4),
-                    )
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.asset("assets/images/home.png", width: 73, height: 29),
-                    Image.asset("assets/images/clipboard.png", width: 28, height: 29),
-                    Image.asset("assets/images/notification.png", width: 33, height: 33),
-                    Image.asset("assets/images/Test Account.png", width: 25, height: 25),
-                  ],
+              top: 7,
+              left: 15,
+              child: GestureDetector(
+                onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Scaffold(
+                    appBar: AppBar(title: Text('Account Page')),
+                    body: Center(child: Text('Account Page Placeholder')),
+                  )),
+                );
+
+                },
+                child: Image.asset(
+                  "assets/images/account.png",
+                  width: 40,
+                  height: 40,
                 ),
               ),
             ),
@@ -158,38 +157,49 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
     );
   }
 
-  Widget _buildLargeCard(String imagePath) {
-    return Container(
+  Widget _buildExerciseCard() {
+    return Stack(
       clipBehavior: Clip.none,
-      height: 250,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        image: DecorationImage(
-          image: AssetImage(imagePath),
-          fit: BoxFit.cover,
+      children: [
+        Container(
+          height: 158,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+            boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)],
+          ),
+          child: const Center(
+            child: Text(
+              "EXERCISES",
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 39, 19, 94)),
+            ),
+          ),
         ),
-      ),
+        Positioned(top: 0, left: 10, child: Image.asset("assets/images/exercise-image1.png", width: 60.44, height: 59.25)),
+        Positioned(top: 5, right: 30, child: Image.asset("assets/images/exercise-image2.png", width: 30.12, height: 29.73)),
+        Positioned(bottom: 10, left: 10, child: Image.asset("assets/images/exercise-image3.png", width: 30, height: 30)),
+        Positioned(bottom: -35, right: -30, child: Image.asset("assets/images/exercise-image4.png", width: 137, height: 116.53)),
+      ],
     );
   }
 
-  Widget _buildQuizCard(String backgroundPath, String fontPath) {
+  Widget _buildQuizCard() {
     return Stack(
       clipBehavior: Clip.none,
-      alignment: Alignment.center,
       children: [
         Container(
-          height: 100,
+          height: 159,
           decoration: BoxDecoration(
-            color: Colors.white,
             borderRadius: BorderRadius.circular(15),
-            boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 5)],
+            color: Colors.white,
+            boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)],
           ),
-          child: Center(
-            child: Image.asset(backgroundPath, width: 60, height: 60, fit: BoxFit.contain),
+          child: const Center(
+            child: Text(
+              "QUIZ",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 39, 19, 94)),
+            ),
           ),
-        ),
-        Positioned(
-          child: Image.asset(fontPath, width: 80),
         ),
         Positioned(top: 5, left: 5, child: Image.asset("assets/images/quiz-image1.png", width: 38.6, height: 29.34)),
         Positioned(top: 15, left: 27, child: Image.asset("assets/images/quiz-image2.png", width: 22.35, height: 16.01)),
@@ -200,29 +210,28 @@ class _ExerciseHomePageState extends State<ExerciseHomePage> {
     );
   }
 
-  Widget _buildResultCard(String backgroundPath, String fontPath) {
+  Widget _buildResultCard() {
     return Stack(
       clipBehavior: Clip.none,
-      alignment: Alignment.center,
       children: [
         Container(
-          height: 100,
+          height: 159,
           decoration: BoxDecoration(
-            color: Colors.white,
             borderRadius: BorderRadius.circular(15),
-            boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 5)],
+            color: Colors.white,
+            boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10)],
           ),
-          child: Center(
-            child: Image.asset(backgroundPath, width: 60, height: 60, fit: BoxFit.contain),
+          child: const Center(
+            child: Text(
+              "RESULTS",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 39, 19, 94)),
+            ),
           ),
         ),
-        Positioned(
-          child: Image.asset(fontPath, width: 80),
-        ),
-        Positioned(top: 5, left: 5, child: Image.asset("assets/images/result-image.png", width: 50.61, height: 51.75)),
-        Positioned(top: 5, right: 5, child: Image.asset("assets/images/trophy 1.png", width: 55, height: 54)),
-        Positioned(bottom: 5, left: 5, child: Image.asset("assets/images/trophy 2.png", width: 30.94, height: 28.51)),
-        Positioned(bottom: -30, right: -20, child: Image.asset("assets/images/trophy 3.png", width: 75, height: 73)),
+        Positioned(left: 5, top: 5, child: Image.asset("assets/images/result-image.png", width: 50.61, height: 51.75)),
+        Positioned(right: 0, top: 0, child: Image.asset("assets/images/trophy 1.png", width: 55, height: 54)),
+        Positioned(left: 5, bottom: 5, child: Image.asset("assets/images/trophy 2.png", width: 30.94, height: 28.51)),
+        Positioned(right: -18, bottom: -35, child: Image.asset("assets/images/trophy 3.png", width: 75, height: 73)),
       ],
     );
   }
