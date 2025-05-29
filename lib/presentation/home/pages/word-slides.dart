dab03c849971116dc8034034b6767636fbde8b71
PavanCodes05 <pavancodes05@gmail.com>
2025-05-29 21:51:02 +0530

    Minor Changes - Word Slides

--- lib/presentation/home/pages/word-slides.dart
+++ lib/presentation/home/pages/word-slides.dart
@@ -12,7 +12,7 @@ class WordSlides extends StatefulWidget {
     super.key,
     required this.exerciseNumber,
     required this.data,
-    required this.grade
+    required this.grade,
   });
 
   @override
@@ -28,8 +28,10 @@ class _WordSlidesState extends State<WordSlides> with SingleTickerProviderStateM
   late Animation<double> _opacityAnimation;
   late Animation<double> _shakeAnimation;
   late Animation<double> _hyphenAnimation;
+  late FocusNode _focusNode;
+
   bool _isImageReady = false;
-  bool _isKeyboardOpen = false;
+  bool _keyboardVisible = false;
 
   @override
   void initState() {
@@ -37,14 +39,18 @@ class _WordSlidesState extends State<WordSlides> with SingleTickerProviderStateM
     _currentIndex = 0;
     _flutterTts = FlutterTts();
     _controller = TextEditingController();
+    _focusNode = FocusNode();
+
     _animationController = AnimationController(
       duration: const Duration(milliseconds: 500),
       vsync: this,
     );
+
     _offsetAnimation = Tween<Offset>(
       begin: const Offset(1, 0),
       end: Offset.zero,
     ).animate(_animationController);
+
     _opacityAnimation = Tween<double>(
       begin: 0.0,
       end: 1.0,
@@ -52,6 +58,7 @@ class _WordSlidesState extends State<WordSlides> with SingleTickerProviderStateM
       parent: _animationController,
       curve: const Interval(0.5, 1.0, curve: Curves.easeInOut),
     ));
+
     _shakeAnimation = Tween<double>(
       begin: 0.0,
       end: 10.0,
@@ -59,6 +66,7 @@ class _WordSlidesState extends State<WordSlides> with SingleTickerProviderStateM
       parent: _animationController,
       curve: const Interval(0.0, 0.2, curve: Curves.elasticInOut),
     ));
+
     _hyphenAnimation = Tween<double>(
       begin: 0.0,
       end: 1.0,
@@ -72,37 +80,51 @@ class _WordSlidesState extends State<WordSlides> with SingleTickerProviderStateM
 
   @override
   void dispose() {
+    _focusNode.dispose();
     _controller.dispose();
     _animationController.dispose();
     _flutterTts.stop();
     super.dispose();
   }
 
-  void _nextSlide() {
+  void _toggleKeyboard() {
     setState(() {
-      String currentWord = widget.data[_currentIndex]['word']!.toUpperCase();
-      if (_currentIndex < widget.data.length - 1 && _controller.text.toUpperCase() == currentWord) {
-        if (_isImageReady) {
-          _currentIndex++;
-          _controller.clear();
-          _animationController.forward(from: 0.0);
-          _isKeyboardOpen = false;
-        }
-      } else if (_controller.text.toUpperCase() == currentWord) {
-        Provider.of<ProgressProvider>(context, listen: false).markExerciseCompleted("exercise${widget.grade}${widget.exerciseNumber}");
-        Navigator.pop(context);
-
+      _keyboardVisible = !_keyboardVisible;
+      if (_keyboardVisible) {
+        _focusNode.requestFocus();
       } else {
-        _shakeAnimation = Tween<double>(
-          begin: 0.0,
-          end: 3.0,
-        ).animate(CurvedAnimation(
-          parent: _animationController,
-          curve: const Interval(0.0, 0.2, curve: Curves.elasticInOut),
-        ));
-        _animationController.forward(from: 0.0);
+        _focusNode.unfocus();
       }
     });
+  }
+
+  void _nextSlide() {
+    String currentWord = widget.data[_currentIndex]['word']!.toUpperCase();
+
+    if (_controller.text.toUpperCase() == currentWord) {
+      if (_currentIndex < widget.data.length - 1) {
+        if (_isImageReady) {
+          setState(() {
+            _currentIndex++;
+            _controller.clear();
+            _animationController.forward(from: 0.0);
+          });
+        }
+      } else {
+        Provider.of<ProgressProvider>(context, listen: false)
+            .markExerciseCompleted("exercise${widget.grade}${widget.exerciseNumber}");
+        Navigator.pop(context);
+      }
+    } else {
+      _shakeAnimation = Tween<double>(
+        begin: 0.0,
+        end: 3.0,
+      ).animate(CurvedAnimation(
+        parent: _animationController,
+        curve: const Interval(0.0, 0.2, curve: Curves.elasticInOut),
+      ));
+      _animationController.forward(from: 0.0);
+    }
   }
 
   void _speakWord() async {
@@ -116,149 +138,170 @@ class _WordSlidesState extends State<WordSlides> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     String currentWord = widget.data[_currentIndex]['word']!.toUpperCase();
     String userInput = _controller.text.toUpperCase();
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Exercise-${widget.exerciseNumber}',
-              style: const TextStyle(fontSize: 44, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            widget.data[_currentIndex]['picture'] != null
-                ? FadeTransition(
-                    opacity: _opacityAnimation,
-                    child: SlideTransition(
-                      position: _offsetAnimation,
-                      child: Transform.translate(
-                        offset: Offset(_shakeAnimation.value, 0),
-                        child: Image.network(
-                          widget.data[_currentIndex]['picture']!,
-                          width: 200,
-                          height: 200,
-                          fit: BoxFit.contain,
-                          loadingBuilder: (context, child, loadingProgress) {
-                            if (loadingProgress == null) {
-                              _isImageReady = true;
-                              return child;
-                            } else {
-                              _isImageReady = false;
-                              return const CircularProgressIndicator();
-                            }
-                          },
-                          errorBuilder: (context, error, stackTrace) {
-                            _isImageReady = false;
-                            return const Text('Image not available');
-                          },
-                        ),
-                      ),
-                    ),
-                  )
-                : const SizedBox.shrink(),
-            const SizedBox(height: 20),
-            Text(
-              currentWord,
-              style: const TextStyle(fontSize: 44, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            GestureDetector(
-              onTap: () {
-                if (!_isKeyboardOpen) {
-                  FocusScope.of(context).requestFocus(FocusNode());
-                  setState(() {
-                    _isKeyboardOpen = true;
-                  });
-                }
-              },
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(currentWord.length, (index) {
-                      return Container(
-                        width: 40,
-                        margin: const EdgeInsets.symmetric(horizontal: 6),
-                        child: Column(
-                          children: [
-                            Text(
-                              index < userInput.length ? userInput[index] : '',
-                              style: const TextStyle(
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const SizedBox(height: 4),
-                            ScaleTransition(
-                              scale: _hyphenAnimation,
-                              child: Container(
-                                height: 10,
-                                color: Colors.black54,
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
-                  ),
-                  Opacity(
-                    opacity: _isKeyboardOpen ? 1.0 : 0.0,
-                    child: TextField(
-                      controller: _controller,
-                      maxLength: currentWord.length,
-                      autofocus: _isKeyboardOpen,
-                      keyboardType: TextInputType.text,
-                      onChanged: (value) {
-                        setState(() {
-                          _controller.value = _controller.value.copyWith(
-                            text: value.toUpperCase(),
-                            selection: TextSelection.collapsed(offset: value.length),
-                          );
-                        });
-                      },
-                    ),
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          child: Center(
+            child: Column(
               children: [
-                GestureDetector(
-                  onTap: _speakWord,
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Image.asset('assets/images/volume.png'),
+                Text(
+                  'Exercise-${widget.exerciseNumber}',
+                  style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                widget.data[_currentIndex]['picture'] != null
+                    ? FadeTransition(
+                        opacity: _opacityAnimation,
+                        child: SlideTransition(
+                          position: _offsetAnimation,
+                          child: Transform.translate(
+                            offset: Offset(_shakeAnimation.value, 0),
+                            child: Image.network(
+                              widget.data[_currentIndex]['picture']!,
+                              width: screenWidth * 0.6,
+                              height: screenWidth * 0.6,
+                              fit: BoxFit.contain,
+                              loadingBuilder: (context, child, loadingProgress) {
+                                if (loadingProgress == null) {
+                                  _isImageReady = true;
+                                  return child;
+                                } else {
+                                  _isImageReady = false;
+                                  return const CircularProgressIndicator();
+                                }
+                              },
+                              errorBuilder: (context, error, stackTrace) {
+                                _isImageReady = false;
+                                return const Text('Image not available');
+                              },
+                            ),
+                          ),
+                        ),
+                      )
+                    : const SizedBox.shrink(),
+                const SizedBox(height: 20),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    currentWord,
+                    style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                const SizedBox(width: 20),
-                GestureDetector(
-                  onTap: _nextSlide,
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
+                const SizedBox(height: 30),
+
+                /// Hyphen + visible keyboard toggle
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap: _toggleKeyboard,
+                      child: Wrap(
+                        alignment: WrapAlignment.center,
+                        spacing: 6,
+                        children: List.generate(currentWord.length, (index) {
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                index < userInput.length ? userInput[index] : '',
+                                style: const TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              const SizedBox(height: 4),
+                              ScaleTransition(
+                                scale: _hyphenAnimation,
+                                child: Container(
+                                  width: 30,
+                                  height: 6,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          );
+                        }),
+                      ),
                     ),
-                    child: Image.asset('assets/images/next.png'),
-                  ),
+
+                    const SizedBox(height: 12),
+
+                    // 🔘 Icon toggle for keyboard
+                    IconButton(
+                      onPressed: _toggleKeyboard,
+                      icon: Icon(
+                        _keyboardVisible ? Icons.keyboard_hide : Icons.keyboard,
+                        color: Colors.black54,
+                      ),
+                      iconSize: 28,
+                      tooltip: _keyboardVisible ? 'Hide Keyboard' : 'Show Keyboard',
+                    ),
+
+
+                    // 🔤 Hidden Input
+                    SizedBox(
+                      width: 0,
+                      height: 0,
+                      child: TextField(
+                        focusNode: _focusNode,
+                        controller: _controller,
+                        maxLength: currentWord.length,
+                        keyboardType: TextInputType.text,
+                        style: const TextStyle(color: Colors.transparent),
+                        cursorColor: Colors.transparent,
+                        showCursor: false,
+                        decoration: const InputDecoration.collapsed(hintText: ""),
+                        onChanged: (value) {
+                          setState(() {
+                            _controller.value = _controller.value.copyWith(
+                              text: value.toUpperCase(),
+                              selection: TextSelection.collapsed(offset: value.length),
+                            );
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: _speakWord,
+                      child: Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Image.asset('assets/images/volume.png'),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    GestureDetector(
+                      onTap: _nextSlide,
+                      child: Container(
+                        width: 70,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Image.asset('assets/images/next.png'),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
-
-
-
